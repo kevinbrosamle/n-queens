4d75bc058fbc0ae8017ec9daf7376be231c488c8
kevinbrosamle <kevin.brosamle@gmail.com>
2016-08-03 20:06:46 -0700

    clipboard

---
+++ clipboard.js
@@ -0,0 +1,19 @@
+// var searchBoard = function(n, x, y) {
+  //   if (n === 0) {
+  //     solutionCount++;
+  //     return;
+  //   }
+
+  //   board.togglePiece(x, y);
+    
+  //   if (!board.hasAnyRooksConflicts()) {
+  //     for (var i = 0; i < board.attributes.n; i++) {
+  //       for (var j = 0; j < board.attributes.n; j++) {
+          
+  //         searchBoard(n - 1, i, j);
+  //       }
+  //     } 
+  //   }
+  // };
+
+  // searchBoard(n, 0, 0);
--- src/solvers.js
+++ src/solvers.js
@@ -24,8 +24,6 @@ window.findNRooksSolution = function(n) {
     board.togglePiece(i, i);
   }
 
-
-
   var solution = board.rows();
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
@@ -35,7 +33,10 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solution = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board ({n: n});
+
+  
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
